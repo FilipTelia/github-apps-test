1de55bbe046f130cfc4d1d51fbfaa4f905443a62
FilipTelia <118185705+FilipTelia@users.noreply.github.com>
2023-04-27 12:57:36 +0200

    Update generate-jwt.rb

--- .github/generate-jwt.rb
+++ .github/generate-jwt.rb
@@ -2,18 +2,19 @@ require 'openssl'
 require 'jwt'  # https://rubygems.org/gems/jwt
 
 # Private key contents
-private_pem = File.read("private.pem")
-private_key = OpenSSL::PKey::RSA.new(private_pem)
+# private_pem = File.read("private.pem")
+# private_key = OpenSSL::PKey::RSA.new(private_pem)
 
 # Generate the JWT
-payload = {
+# payload = {
   # issued at time, 60 seconds in the past to allow for clock drift
-  iat: Time.now.to_i - 60,
+#  iat: Time.now.to_i - 60,
   # JWT expiration time (10 minute maximum)
-  exp: Time.now.to_i + (10 * 60),
+#  exp: Time.now.to_i + (10 * 60),
   # GitHub App's identifier
-  iss: "324195"
+#  iss: "324195"
 }
 
-jwt = JWT.encode(payload, private_key, "RS256")
-puts jwt
+#jwt = JWT.encode(payload, private_key, "RS256")
+#puts jwt
+puts "Hello World"
